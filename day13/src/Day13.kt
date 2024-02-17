ec0ba341505ca6c548b11021b0f22098c1662ae8
Simon Wilson <ksattic@gmail.com>
2024-02-17 12:13:34 -1000

    Day 13 part 2 solution

--- day13/src/Day13.kt
+++ day13/src/Day13.kt
@@ -26,8 +26,13 @@ class Day13(path: String) {
         return outputValue
     }
 
-    private fun doesReflect(list: List<Long>): Int {
+    private fun doesReflect(list: List<Long>, exclude: Int): Int {
         for (cut in 0 until list.size - 1) {
+            // Row or column to skip, -1 if none
+            if (exclude != -1 && exclude == cut + 1) {
+                continue
+            }
+
             val a = cut
             val b = cut + 1
             val size = min(list.size - cut - 1, cut + 1)
@@ -45,17 +50,17 @@ class Day13(path: String) {
         return -1
     }
 
-    private fun reflectsHorizontally(block: List<String>): Int {
+    private fun reflectsHorizontally(block: List<String>, exclude: Int = -1): Int {
         val rows = mutableListOf<Long>()
 
         block.forEach {
             rows.add(stringHash(it))
         }
 
-        return doesReflect(rows)
+        return doesReflect(rows, exclude)
     }
 
-    private fun reflectsVertically(block: List<String>): Int {
+    private fun reflectsVertically(block: List<String>, exclude: Int = -1): Int {
         val cols = mutableListOf<Long>()
         val width = block[0].length
         val height = block.size
@@ -68,7 +73,39 @@ class Day13(path: String) {
             cols.add(stringHash(column))
         }
 
-        return doesReflect(cols)
+        return doesReflect(cols, exclude)
+    }
+
+    private fun locateMirrorSmudge(blockIndex: Int, size: Pair<Int, Int>): Int {
+        val block = blocks[blockIndex]
+        val excludeLeft = originalReflection[blockIndex].first
+        val excludeAbove = originalReflection[blockIndex].second
+
+        // Try every combo
+        for (y in 0 until size.second) {
+            for (x in 0 until size.first) {
+                val inverse = when (block[y][x]) {
+                    '#' -> "."
+                    else -> "#"
+                }
+
+                val newBlock = block.toMutableList()
+                newBlock[y] = newBlock[y].replaceRange(x, x + 1, inverse)
+
+                val left = reflectsVertically(newBlock, excludeLeft)
+                if (left != -1) {
+                    // Stop when found
+                    return left
+                }
+                val above = reflectsHorizontally(newBlock, excludeAbove)
+                if (above != -1) {
+                    return above * 100
+                }
+            }
+        }
+
+        // None found
+        return -1
     }
 
     fun part1(): Long {
@@ -86,6 +123,8 @@ class Day13(path: String) {
 
             val width = b[0].length
             val height = b.size
+
+            // Stash these for part 2
             sizes.add(Pair(width, height))
             originalReflection.add(Pair(left, above))
         }
@@ -94,16 +133,17 @@ class Day13(path: String) {
     }
 
     fun part2(): Long {
+        var sum = 0L
+
         // Try each block with an inverted # or .
-        sizes.forEachIndexed { i, s ->
-            for (x in 0 until s.first) {
-                for (y in 0 until s.second) {
-                    val b = blocks[i]
-                    val originalChar = b[y][x]
-                }
+        blocks.indices.forEach { i ->
+            val ret = locateMirrorSmudge(i, sizes[i])
+            if (ret != -1) {
+                sum += ret
             }
         }
-        return 0L
+
+        return sum
     }
 }
 
